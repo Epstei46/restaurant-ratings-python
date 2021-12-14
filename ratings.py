69faa7573a0b6462e872f8422addb2f483ba708c
Steven Epstein <sbepstein42@gmail.com>
2021-12-13 21:21:29 -0500

    Made some bonus changes

--- ratings.py
+++ ratings.py
@@ -1,5 +1,6 @@
 """Restaurant rating lister."""
 # Exercise located at https://ed.devmountain.com/materials/data-bp-1/exercises/core-python/
+# If I come back to this, didn't finish all the further study stuff.
 
 
 def process_scores():
@@ -25,7 +26,18 @@ def add_restaurant(dictionary):
 
     user_key = input("Add a restuarant name: ")
     user_value = input("Add a rating: ")
-    dictionary[user_key] = int(user_value)
+    while True: # loop to validate restaurant rating user just inputted.
+        try:
+            user_value = int(user_value)
+            if 0 < user_value < 6:
+                dictionary[user_key] = int(user_value)
+                return False
+            else:
+                user_value = input("Rating must be a number between 1 and 5. Try again: ")
+                continue
+        except:
+            user_value = input("Rating must be a number between 1 and 5. Try again: ")
+            continue
     
 
 def print_sorted_dictionary(dictionary):
@@ -35,12 +47,23 @@ def print_sorted_dictionary(dictionary):
     # print(sorted_dict)
     for key,value in sorted(dictionary.items()):
         print(f"{key} is rated at {value}.")
+        
+def what_do():
+    """"Asks the user what they would like to do. View restaurants + ratings, add to the list, or quit."""
+    # read existing scores in from file
+    scores = process_scores()
+    while True:
+        user_input = input("What would you like to do? Enter '1' to see all ratings, enter '2' to add and rate a new restaurant, and enter '3' to Quit.\n")
+        if user_input == '1':
+            # print an alphabetical list of all rated restaurants and their ratings
+            print_sorted_dictionary(scores)
+        elif user_input == '2':
+            # allow user to add a restaurant/rating pair
+            add_restaurant(scores)
+        elif user_input == '3':
+            return False
+        else:
+            user_input = input(f"Mate. '{user_input}' is invalid. Enter '1' to see all ratings, enter '2' to add and rate a new restaurant, and enter '3' to Quit.\n")
 
-# read existing scores in from file
-scores = process_scores()
-
-# allow user to add a restaurant/rating pair
-add_restaurant(scores)
-
-# print an alphabetical list of all rated restaurants and their ratings
-print_sorted_dictionary(scores)
+# Initiates the app.
+what_do()
